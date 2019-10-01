6857dcee4d01aa3c2fe212d35fc2bfe226eadf7f
VimForLaNie <new.giant098@gmaill.com>
2019-10-01 14:03:15 +0700

    Merge Sort

--- c++/Insertion Sort.cpp
+++ c++/Insertion Sort.cpp
@@ -1,42 +1,38 @@
 #include <stdio.h>
+#include <cstdlib>
 
 int main(){
     //variables
-    int P,pointer = 1;
+    int count = 0;
+    int pointer = 1;
     int size;
     //input
     printf("Insert size of Array : ");
     scanf("%d",&size);
-    
+    printf("size : %d\n",size);
     int array[size];
-
     for (int i = 0; i < size; i++){
-        printf("Insert The %d element of array : ");
-        scanf("%d",array[i]);
+        array[i] = rand();
+        printf("Array @%d : %d\n",i,array[i]);
     }
-    printf("\n");
     //sorting
-    while(P < size){
-        while(pointer <= P){
-            if(array[pointer] > array[pointer - 1]){
+    for(int P = 1; P <= size; P++){
+        while(pointer <= P && pointer != 0){
+            if(array[pointer] < array[pointer - 1]){
+                count++;
+                printf("Comparing. . . [%d]\n",count);
                 int holder = array[pointer];
                 array[pointer] = array[pointer - 1];
                 array[pointer - 1] = holder;
             }
             pointer--;
         }
-        P++;
         pointer = P;
     }
-    //printing
-    printf("Your Array : ");
-    for(int i = 0; i < size; i++){
-        printf("%d\t",array[i]);
-    }
-    printf("\n");
     printf("Sorted Array : ");
     for(int i = 0; i < size; i++){
-        printf("%d\t",array[i]);
+        printf("%d ",array[i]);
     }
     printf("\n");
+    return 0;
 }
