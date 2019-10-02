9bb82ae45d13b0fd6d4e3dc0d9d3da4841ec6135
VimForLaNie <new.giant098@gmaill.com>
2019-10-02 19:40:41 +0700

    Minor Adjustment

--- c++/Speedrun.cpp
+++ c++/Speedrun.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <time.h>
 
 int main(){
+    double time_spent = 0.0;
     int totalTime = 0;
     int nstage = 0;
     int nvid = 0;
@@ -21,74 +23,80 @@ int main(){
         pos[i] = 0;
     }
     int table[nvid][2];
-    int gaytor = 1;
+    // int gaytor = 1;
     for (int i = 0; i < nvid; i++)
     {
-        if(gaytor > nstage){
-            gaytor = 1;
-        }
-        table[i][0] = gaytor;
-        table[i][1] = rand() % 501;
-        gaytor++;
+        scanf("%d %d",table[i][0],table[i][0]);
+        // if(gaytor > nstage){
+        //     gaytor = 1;
+        // }
+        // table[i][0] = gaytor;
+        // table[i][1] = rand() % 501;
+        // gaytor++;
+
     }
-    printf("------------------------------------\n");
-    printf("Table :");
-    for (int i = 0; i < nvid; i++)
-    {
-        for (int x = 0; x < 2; x++)
-        {
-            printf("\t%d",table[i][x]);
-        }
-        printf("\n");
-    }
-    printf("------------------------------------\n");
+    clock_t start = clock(); 
+    // printf("------------------------------------\n");
+    // printf("Table :");
+    // for (int i = 0; i < nvid; i++)
+    // {
+    //     for (int x = 0; x < 2; x++)
+    //     {
+    //         printf("\t%d",table[i][x]);
+    //     }
+    //     printf("\n");
+    // }
+    // printf("------------------------------------\n");
     for (int count = 1; count <= nstage; count++)
     {
         for (int i = 0; i < nvid; i++)
         {
             if(table[i][0] == count){
-                printf("table[%d][0] == %d == True\n",i,count);
+                // printf("table[%d][0] == %d == True\n",i,count);
                 for (int x = 0; x < nvid; x++)
                 {
-                    printf("Finding Min . . . [%d]\n",x);
+                    // printf("Finding Min . . . [%d]\n",x);
                     if(table[x][1] < min[count - 1] && table[x][0] == count){
-                        printf("\ttable[%d][1] < %d && table[%d][0] == %d True\n",x,min[count - 1],x,count);
+                        // printf("\ttable[%d][1] < %d && table[%d][0] == %d True\n",x,min[count - 1],x,count);
                         min[count - 1] = table[x][1];
-                        printf("\tmin[%d] = %d -> table[%d][1]\n",count - 1,min[count - 1],x);
-                        printf("\tmin :");
-                        for (int i = 0; i < nstage; i++)
-                        {
-                            printf(" %d",min[i]);
-                        }
-                        printf("\n");
+                        // printf("\tmin[%d] = %d -> table[%d][1]\n",count - 1,min[count - 1],x);
+                        // printf("\tmin :");
+                        // for (int i = 0; i < nstage; i++)
+                        // {
+                        //     printf(" %d",min[i]);
+                        // }
+                        // printf("\n");
                         pos[count - 1] = x;
-                        printf("*_=_*pos = %d\n",x);
+                        // printf("*_=_*pos = %d\n",x);
                     }
                 }
-                printf("------------------------------------\n");
+                // printf("------------------------------------\n");
                 break; 
             }
         }
     }  
-    printf("min :");
+    // printf("min :");
     for (int i = 0; i < nstage; i++)
     {
         totalTime += min[i];
-        printf(" %d",min[i]);
+        // printf(" %d",min[i]);
     }
-    printf("\n");
-    printf("pos :");
+    // printf("\n");
+    // printf("pos :");
     for (int i = 0; i < nstage; i++)
     {
-        printf(" %d",pos[i]);
+        // printf(" %d",pos[i]);
     }
-    printf("\n");
-    printf("------------------------------------\n");
+    // printf("\n");
+    // printf("------------------------------------\n");
     printf("%d\n",totalTime);
     for (int i = 0; i < nstage; i++)
     {
         printf("%d %d\n",table[pos[i]][0],table[pos[i]][1]);
     }
+    clock_t end = clock();
+    time_spent += (double)(end - start) / CLOCKS_PER_SEC;
+    printf("time used in calculation : %f",time_spent);
     scanf(" %c");
     return 0;
 }
